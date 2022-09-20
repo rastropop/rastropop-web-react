142c255c81b4c1cf26d8c082d20cb7f7dfbc45cd
RodrigoAndrade085 <rodrigoandradedsousa@gmail.com>
2022-09-20 19:53:27 -0300

    initial commit

---
+++ src/menu-items/index.tsx
@@ -0,0 +1,13 @@
+// project import
+import other from './other';
+
+// types
+import { NavItemType } from 'types';
+
+// ==============================|| MENU ITEMS ||============================== //
+
+const menuItems: { items: NavItemType[] } = {
+    items: [other]
+};
+
+export default menuItems;
