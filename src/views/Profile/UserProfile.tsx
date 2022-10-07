86c86509c3347c8e4fea3da7c7bc477ff4ec67a3
RodrigoAndrade085 <rodrigoandradedsousa@gmail.com>
2022-10-06 21:22:42 -0300

    população de campos

--- src/views/Profile/UserProfile.tsx
+++ src/views/Profile/UserProfile.tsx
@@ -7,51 +7,54 @@ import SubCard from 'ui-component/cards/SubCard';
 import AnimateButton from 'ui-component/extended/AnimateButton';
 import { gridSpacing } from 'store/constant';
 
+import useAuth from 'hooks/useAuth';
+
 // assets
 
-const UserProfile = () => (
-    // const { user } = useAuth();
+const UserProfile = () => {
+    const { user } = useAuth();
+    return (
+        <Grid container spacing={gridSpacing}>
+            <Grid item sm={6} md={8}>
+                <SubCard title="Perfil">
+                    <Grid container spacing={gridSpacing}>
+                        <Grid item md={6} xs={12}>
+                            <TextField id="outlined-basic" fullWidth label="Nome" value={user!.name} />
+                        </Grid>
+                        <Grid item md={6} xs={12}>
+                            <TextField id="outlined-basic" fullWidth label="Endereço" defaultValue="Rua 1, 1" value={user!.address} />
+                        </Grid>
+                        <Grid item md={6} xs={12}>
+                            <TextField id="outlined-basic" fullWidth label="Email" defaultValue="name@example.com" value={user!.email} />
+                        </Grid>
+                        <Grid item md={6} xs={12}>
+                            <TextField id="outlined-basic" fullWidth label="Registrado em" defaultValue="**/**/****" />
+                        </Grid>
+                        <Grid item md={6} xs={12}>
+                            <TextField id="outlined-basic" fullWidth label="Telefone" defaultValue="(085) 99905-2977" value={user!.phone} />
+                        </Grid>
+                        <Grid item md={6} xs={12}>
+                            <TextField id="outlined-basic" fullWidth label="CPF" defaultValue="000.000.000-00" value={user!.cpf} />
+                        </Grid>
+                        <Grid item md={6} xs={12}>
+                            <TextField id="outlined-basic" fullWidth label="CNPJ" defaultValue="00.000.000/0001-00" value={user!.cnpj} />
+                        </Grid>
+                        <Grid item md={6} xs={12}>
+                            <TextField id="outlined-basic" fullWidth label="RG" defaultValue="0000000000000" value={user!.rg} />
+                        </Grid>
 
-    <Grid container spacing={gridSpacing}>
-        <Grid item sm={6} md={8}>
-            <SubCard title="Perfil">
-                <Grid container spacing={gridSpacing}>
-                    <Grid item md={6} xs={12}>
-                        <TextField id="outlined-basic" fullWidth label="Nome" defaultValue="Rodrigo Andrade" />
+                        <Grid item xs={12}>
+                            <Stack direction="row">
+                                <AnimateButton>
+                                    <Button variant="contained">Salvar</Button>
+                                </AnimateButton>
+                            </Stack>
+                        </Grid>
                     </Grid>
-                    <Grid item md={6} xs={12}>
-                        <TextField id="outlined-basic" fullWidth label="Endereço" defaultValue="Rua 1, 1" />
-                    </Grid>
-                    <Grid item md={6} xs={12}>
-                        <TextField id="outlined-basic" fullWidth label="Email" defaultValue="name@example.com" />
-                    </Grid>
-                    <Grid item md={6} xs={12}>
-                        <TextField id="outlined-basic" fullWidth label="Registrado em" defaultValue="01/06/2018" />
-                    </Grid>
-                    <Grid item md={6} xs={12}>
-                        <TextField id="outlined-basic" fullWidth label="Telefone" defaultValue="(085) 99905-2977" />
-                    </Grid>
-                    <Grid item md={6} xs={12}>
-                        <TextField id="outlined-basic" fullWidth label="CPF" defaultValue="000.000.000-00" />
-                    </Grid>
-                    <Grid item md={6} xs={12}>
-                        <TextField id="outlined-basic" fullWidth label="CNPJ" defaultValue="00.000.000/0001-00" />
-                    </Grid>
-                    <Grid item md={6} xs={12}>
-                        <TextField id="outlined-basic" fullWidth label="RG" defaultValue="0000000000000" />
-                    </Grid>
-
-                    <Grid item xs={12}>
-                        <Stack direction="row">
-                            <AnimateButton>
-                                <Button variant="contained">Salvar</Button>
-                            </AnimateButton>
-                        </Stack>
-                    </Grid>
-                </Grid>
-            </SubCard>
+                </SubCard>
+            </Grid>
         </Grid>
-    </Grid>
-);
+    );
+};
 
 export default UserProfile;
